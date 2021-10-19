7c8b749550dee081b1e0707699af83a81c2ceb6e
Muskan Valmikee <83203071+MuskanValmiki@users.noreply.github.com>
2021-10-19 12:44:26 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,29 @@
+num=int(input("enter how many customer are in line"))
+i=1
+time_list=[]
+while i<=num:
+    order,preparation=input("enter your order and preparation time=").split()
+    time=int(order)+int(preparation)
+    time_list.append(time)
+    i+=1
+index=0
+count=0
+while index<len(time_list):
+    if time_list[index] in time_list:
+        count+=1
+    else:
+        pass
+    index+=1
+if count>1:
+    j=0
+    while j<len(time_list):
+        k=0
+        while k<len(time_list):
+            if time_list[j]<time_list[k]:
+                time_list[j],time_list[k]=time_list[k],time_list[j]
+            k+=1
+        j+=1
+    print(time_list)
+else:
+    print(time_list)
+# jim anti order hackerrank question 
